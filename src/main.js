e279bc13ce78db92e3951b7b64972433e539e388
贺齐 <2091287214@qq.com>
2021-05-10 21:28:46 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+
+// 启动人口
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 加载全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
 
+// 创建vue根实例
+// 把router配置到根实例中
+// 通过render把app根组件渲染到#app人口节点
 new Vue({
   router,
   render: h => h(App)
+  // el:'#app' == 等价于$mount('#app)
 }).$mount('#app')
